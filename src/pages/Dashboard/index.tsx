42aa0f756f1b752175e18554c14131023850d318
gaiake12 <gaiake12@hotmail.com>
2020-07-30 17:36:32 -0300

    Challange Done

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from '../../components/Header';
+
+import api from '../../services/api';
+
+import Food from '../../components/Food';
+import ModalAddFood from '../../components/ModalAddFood';
+import ModalEditFood from '../../components/ModalEditFood';
+
+import { FoodsContainer } from './styles';
+
+interface IFoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+const Dashboard: React.FC = () => {
+  const [foods, setFoods] = useState<IFoodPlate[]>([]);
+  const [editingFood, setEditingFood] = useState<IFoodPlate>({} as IFoodPlate);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  useEffect(() => {
+    async function loadFoods(): Promise<void> {
+      // TODO LOAD FOODS
+
+      const response = await api.get('/foods');
+
+      const availableFoods = response.data.filter(
+        (food: IFoodPlate) => food.available,
+      );
+
+      setFoods(availableFoods);
+    }
+
+    loadFoods();
+  }, []);
+
+  async function handleAddFood(
+    food: Omit<IFoodPlate, 'id' | 'available'>,
+  ): Promise<void> {
+    try {
+      // TODO ADD A NEW FOOD PLATE TO THE API
+
+      const { name, description, price, image } = food;
+
+      const response = await api.post('/foods', {
+        name,
+        description,
+        price,
+        image,
+        available: true,
+      });
+
+      setFoods([...foods, response.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleUpdateFood(
+    food: Omit<IFoodPlate, 'id' | 'available'>,
+  ): Promise<void> {
+    // TODO UPDATE A FOOD PLATE ON THE API
+
+    const newFoods = foods.filter(Findfood => Findfood.id !== editingFood.id);
+    console.log(newFoods);
+
+    const { name, description, price, image } = food;
+
+    const newFood = {
+      id: editingFood.id,
+      name: name || editingFood.name,
+      image: image || editingFood.image,
+      price: price || editingFood.price,
+      description: description || editingFood.description,
+      available: editingFood.available,
+    };
+
+    await api.post('/foods', newFood);
+
+    setFoods([...newFoods, newFood]);
+  }
+
+  async function handleDeleteFood(id: number): Promise<void> {
+    // TODO DELETE A FOOD PLATE FROM THE API
+    const newFoods = foods.filter(Findfood => Findfood.id !== id);
+
+    setFoods(newFoods);
+  }
+
+  function toggleModal(): void {
+    setModalOpen(!modalOpen);
+  }
+
+  function toggleEditModal(): void {
+    setEditModalOpen(!editModalOpen);
+  }
+
+  function handleEditFood(food: IFoodPlate): void {
+    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    toggleEditModal();
+    setEditingFood(food);
+  }
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map(food => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+};
+
+export default Dashboard;
